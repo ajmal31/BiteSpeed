5329281c747f60555c957bdbd118bf037eba3e46
lieper-backend <lieperbackend@gmail.com>
2024-07-21 01:35:27 +0530

    Feat : find primary contact

---
+++ src/Controllers/user.ts
@@ -0,0 +1,33 @@
+import { findPrimaryContact } from "../Helpers/user"
+
+const identifyContact = async(email?: string, phoneNumber?: string) => {
+
+
+    try {
+
+        if (!email && !phoneNumber) throw new Error("Please provide neccessory details")
+
+        let primaryAccount = null
+        // If email exists in Request take that contact details if the contact is exists
+        if (email) {
+            const key="email",
+            primaryAccount = await findPrimaryContact(key,email)
+        }
+        
+        //If primary account details not found using email find using phoneNumber
+        if(!primaryAccount&&phoneNumber){
+
+            const key="phoneNumber"
+            primaryAccount=await findPrimaryContact(key,phoneNumber)
+        }
+        // If account details not found regarding number or email create a new one
+        if(!primaryAccount){
+
+            // primaryAccount=await 
+        }
+    } catch (error) {
+
+
+
+    }
+}
